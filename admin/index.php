71c07fa11d6d408d6d12c786706dcdbfdcc201e9
Datpvm2150 <phamvanmanhdat25102005@gmail.com>
2025-04-08 23:32:59 +0700

    Cập nhật quản lý danh mục

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,16 @@
 <?php 
-
+session_start();
 // Require file Common
 require_once '../commons/env.php'; // Khai báo biến môi trường
 require_once '../commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
-require_once 'controllers/DashboardController.php';
+require_once 'controllers/AdminDanhMucController.php';
+require_once 'controllers/AdminBaoCaoThongKeController.php';
+
 
 // Require toàn bộ file Models
+require_once './models/AdminDanhMuc.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
@@ -15,6 +18,15 @@ $act = $_GET['act'] ?? '/';
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
 match ($act) {
-    // Dashboards
-    '/'                 => (new DashboardController())->index(),
+    //Trang chủ - báo cáo thống kê
+
+    '/'=>(new AdminBaoCaoThongKeController())->home(),
+
+    // route danh mục
+    'danh-muc'=> (new AdminDanhMucController())->danhSachDanhMuc(),
+    'form-them-danh-muc'=> (new AdminDanhMucController())->formAddDanhMuc(),
+    'them-danh-muc'=> (new AdminDanhMucController())->postAddDanhMuc(),
+    'form-sua-danh-muc'=> (new AdminDanhMucController())->formEditDanhMuc(),
+    'sua-danh-muc'=> (new AdminDanhMucController())->postEditDanhMuc(),
+    'xoa-danh-muc'=> (new AdminDanhMucController())->deleteDanhMuc(),
 };
