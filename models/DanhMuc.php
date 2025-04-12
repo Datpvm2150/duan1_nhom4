d8d3f17120a185c2fd6489966ffe16437840ee86
QuachVietDuong <duongquach2207@gmail.com>
2025-04-12 08:40:31 +0700

    update danh mục + sp trong models

---
+++ models/DanhMuc.php
@@ -0,0 +1,21 @@
+<?php 
+
+class DanhMuc {
+    public $conn;// Khai báo phương thức
+
+    public function __construct()
+    {
+        $this->conn = connectDB();
+    }
+    
+    public function getAllDanhMuc(){
+        try {
+            $sql = 'SELECT * FROM danh_mucs';
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            echo "Lỗi" . $e->getMessage();
+        }
+    }
+}
