d5bee961e7971e4e15ba0356e4be50fe0365d587
Datpvm2150 <phamvanmanhdat25102005@gmail.com>
2025-04-21 00:12:35 +0700

    Update AdminDonHang, views/donhang, views/home, views/taikhoan

--- admin/index.php
+++ admin/index.php
@@ -1,26 +1,29 @@
 <?php 
 session_start();
+
 // Require file Common
 require_once '../commons/env.php'; // Khai báo biến môi trường
 require_once '../commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
 require_once 'controllers/AdminDanhMucController.php';
-require_once 'controllers/AdminBaoCaoThongKeController.php';
 require_once 'controllers/AdminSanPhamController.php';
+require_once 'controllers/AdminDonHangController.php';
+require_once 'controllers/AdminBaoCaoThongKeController.php';
+require_once 'controllers/AdminTaiKhoanController.php';
 
 // Require toàn bộ file Models
 require_once './models/AdminDanhMuc.php';
-require_once './models/AdminBaoCaoThongKe.php';
 require_once './models/AdminSanPham.php';
-
+require_once './models/AdminDonHang.php';
+require_once './models/AdminTaiKhoan.php';
+require_once './models/AdminBaoCaoThongKe.php'; // Thêm model mới
 
 // Route
 $act = $_GET['act'] ?? '/';
 
-// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
-
-match ($act) {
+// Để bảo đảm tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+match($act){
     // Trang chủ - báo cáo thống kê
     '/' => (new AdminBaoCaoThongKeController())->home(),
     'bao-cao-thong-ke' => (new AdminBaoCaoThongKeController())->home(), // Thêm route phụ
@@ -43,5 +46,30 @@ match ($act) {
     'xoa-san-pham' => (new AdminSanPhamController())->deleteSanPham(),
     'chi-tiet-san-pham' => (new AdminSanPhamController())->detailSanPham(),
 
-    
+    // Route bình luận
+    'an-binh-luan' => (new AdminSanPhamController())->anBinhLuan(),
+    'hien-binh-luan' => (new AdminSanPhamController())->hienBinhLuan(), 
+    'xoa-binh-luan' => (new AdminSanPhamController())->xoaBinhLuan(),
+
+    // Route quản lý đơn hàng
+    'don-hang' => (new AdminDonHangController())->danhSachDonHang(),
+    'form-sua-don-hang' => (new AdminDonHangController())->formEditDonHang(),
+    'sua-don-hang' => (new AdminDonHangController())->postEditDonHang(),
+    'chi-tiet-don-hang' => (new AdminDonHangController())->detailDonHang(),
+
+    // Route quản lý tài khoản
+    'tai-khoan-quan-tri' => (new AdminTaiKhoanController())->danhSachQuanTri(),
+    'form-them-quan-tri' => (new AdminTaiKhoanController())->formAddQuanTri(),
+    'them-quan-tri' => (new AdminTaiKhoanController())->postAddQuanTri(),
+    'form-sua-quan-tri' => (new AdminTaiKhoanController())->formEditQuanTri(),
+    'sua-quan-tri' => (new AdminTaiKhoanController())->postEditQuanTri(),
+    'reset-password' => (new AdminTaiKhoanController())->resetPassword(),
+    'tai-khoan-khach-hang' => (new AdminTaiKhoanController())->danhSachKhachHang(),
+    'form-sua-khach-hang' => (new AdminTaiKhoanController())->formEditKhachHang(),
+    'sua-khach-hang' => (new AdminTaiKhoanController())->postEditKhachHang(),
+    'chi-tiet-khach-hang' => (new AdminTaiKhoanController())->detailKhachHang(),
+    'login-admin' => (new AdminTaiKhoanController())->formLogin(),
+    'check-login-admin' => (new AdminTaiKhoanController())->login(),
+
+    default => die("Hành động không hợp lệ: '{$_GET['act']}'"), // In lỗi
 };
